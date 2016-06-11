5d958333217df3ff88bb42db780a734cdefc13c5
Vitali Sobennicov <vsobennicov@apple.com>
2016-06-10 17:27:40 -0700

    Spaces win

--- Arduino Garage Opener/devicetypes/vint83/arduino_garage_door_opener.src/arduino_garage_door_opener.groovy
+++ Arduino Garage Opener/devicetypes/vint83/arduino_garage_door_opener.src/arduino_garage_door_opener.groovy
@@ -13,58 +13,58 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
- 
+
 import java.security.MessageDigest;
 
- preferences {
-	input("ip", "text", title: "IP Address", description: "ip")
-	input("port", "text", title: "Port", description: "port")
-	input("mac", "text", title: "MAC Addr", description: "mac")
+preferences {
+    input("ip", "text", title: "IP Address", description: "ip")
+    input("port", "text", title: "Port", description: "port")
+    input("mac", "text", title: "MAC Addr", description: "mac")
     input("password", "password", title: "Password", description: "password")
 }
 
 metadata {
-	definition (name: "Arduino Garage Door Opener", namespace: "vint83", author: "vint83") {
-		capability "Actuator"
-		capability "Door Control"
+    definition (name: "Arduino Garage Door Opener", namespace: "vint83", author: "vint83") {
+        capability "Actuator"
+        capability "Door Control"
         capability "Garage Door Control"
-		capability "Contact Sensor"
-		capability "Refresh"
-		capability "Sensor"
+        capability "Contact Sensor"
+        capability "Refresh"
+        capability "Sensor"
         capability "Polling"
         capability "Temperature Measurement"
-
-	}
-
-	simulator {
-		
-	}
-
-	tiles {
-		standardTile("toggle", "device.door", width: 3, height: 2) {
-			state("closed", label:'${name}', action:"open", icon:"st.doors.garage.garage-closed", backgroundColor:"#79b821", nextState:"opening")
-			state("open", label:'${name}', action:"close", icon:"st.doors.garage.garage-open", backgroundColor:"#ffa81e", nextState:"closing")
-			state("opening", label:'${name}', icon:"st.doors.garage.garage-closed", backgroundColor:"#ffe71e")
-			state("closing", label:'${name}', icon:"st.doors.garage.garage-open", backgroundColor:"#ffe71e")
+        
+    }
+    
+    simulator {
+        
+    }
+    
+    tiles {
+        standardTile("toggle", "device.door", width: 3, height: 2) {
+            state("closed", label:'${name}', action:"open", icon:"st.doors.garage.garage-closed", backgroundColor:"#79b821", nextState:"opening")
+            state("open", label:'${name}', action:"close", icon:"st.doors.garage.garage-open", backgroundColor:"#ffa81e", nextState:"closing")
+            state("opening", label:'${name}', icon:"st.doors.garage.garage-closed", backgroundColor:"#ffe71e")
+            state("closing", label:'${name}', icon:"st.doors.garage.garage-open", backgroundColor:"#ffe71e")
             state("unknown", label:'${name}', icon:"st.doors.garage.garage-unknown")
-			
-		}
-		standardTile("open", "device.door", inactiveLabel: false, decoration: "flat") {
-			state "default", label:'open', action:"open", icon:"st.doors.garage.garage-opening"
-		}
-		standardTile("close", "device.door", inactiveLabel: false, decoration: "flat") {
-			state "default", label:'close', action:"close", icon:"st.doors.garage.garage-closing"
-		}
+            
+        }
+        standardTile("open", "device.door", inactiveLabel: false, decoration: "flat") {
+            state "default", label:'open', action:"open", icon:"st.doors.garage.garage-opening"
+        }
+        standardTile("close", "device.door", inactiveLabel: false, decoration: "flat") {
+            state "default", label:'close', action:"close", icon:"st.doors.garage.garage-closing"
+        }
         standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat") {
-			state "default", label:'', action:"refresh.refresh", icon:"st.secondary.refresh"
-		}
+            state "default", label:'', action:"refresh.refresh", icon:"st.secondary.refresh"
+        }
         valueTile("temperature", "device.temperatureCombined", decoration: "flat") {
-			state "temperature", label:'${currentValue}'
-		}
-
-		main "toggle"
-		details(["toggle", "open", "close", "temperature", "refresh"])
-	}
+            state "temperature", label:'${currentValue}'
+        }
+        
+        main "toggle"
+        details(["toggle", "open", "close", "temperature", "refresh"])
+    }
 }
 
 // gets the address of the hub
@@ -76,7 +76,7 @@ private getCallBackAddress() {
 private getHostAddress() {
     def ip = settings.ip
     def port = settings.port
-
+    
     if (!ip || !port) {
         def parts = device.deviceNetworkId.split(":")
         if (parts.length == 2) {
@@ -88,7 +88,7 @@ private getHostAddress() {
             log.warn "Can't figure out ip and port for device: ${device.id}"
         }
     }
-
+    
     log.debug "Using IP: $ip and port: $port for device: ${device.id}"
     return ip + ":" + port
 }
@@ -101,13 +101,13 @@ private String convertHexToIP(hex) {
     return [convertHexToInt(hex[0..1]),convertHexToInt(hex[2..3]),convertHexToInt(hex[4..5]),convertHexToInt(hex[6..7])].join(".")
 }
 
-private String convertIPtoHex(ipAddress) { 
+private String convertIPtoHex(ipAddress) {
     String hex = ipAddress.tokenize( '.' ).collect {  String.format( '%02x', it.toInteger() ) }.join()
     return hex
 }
 
 private String convertPortToHex(port) {
-	String hexport = port.toString().format( '%04x', port.toInteger() )
+    String hexport = port.toString().format( '%04x', port.toInteger() )
     return hexport
 }
 
@@ -116,30 +116,30 @@ def refresh() {
 }
 
 def sendDoorUnknownEvent() {
-	state.previousDoorState = "unknown"
-	sendEvent(name: "door", value: "unknown", displayed: false)
+    state.previousDoorState = "unknown"
+    sendEvent(name: "door", value: "unknown", displayed: false)
 }
 
 def poll() {
     if(device.deviceNetworkId!=settings.mac) {
-		log.debug "setting device network id to device MAC"
-		device.deviceNetworkId = settings.mac;
+        log.debug "setting device network id to device MAC"
+        device.deviceNetworkId = settings.mac;
     }
     state.previousDoorState = device.currentValue("door")
-	log.debug "Executing 'poll'"
+    log.debug "Executing 'poll'"
     runIn(30, sendDoorUnknownEvent)
-	def hubaction = new physicalgraph.device.HubAction(
-    	method: "GET",
-    	path: "/getstatus",
-    	headers: [
-        	HOST: "${getHostAddress()}"
-    	]
-	)
+    def hubaction = new physicalgraph.device.HubAction(
+        method: "GET",
+        path: "/getstatus",
+        headers: [
+            HOST: "${getHostAddress()}"
+        ]
+    )
     return hubaction
 }
 
 def parse(String description) {
-	//log.trace "parse($description)"
+    //log.trace "parse($description)"
     def msg = parseLanMessage(description)
     
     def status = msg.status          // => http status code of the response
@@ -147,17 +147,17 @@ def parse(String description) {
     log.debug "Received data: ${data}"
     def result = []
     if (status == 200 || status == null) {
-    	//unschedule sendDoorUnknownEvent
-    	unschedule()
+        //unschedule sendDoorUnknownEvent
+        unschedule()
         state.nonce = data?.nonce
         if (!state.previousDoorState) {
-        	state.previousDoorState = device.currentValue("door")
+            state.previousDoorState = device.currentValue("door")
         }
         log.debug "parse previousDoorState: ${state?.previousDoorState}"
         // don't send the event if door status haven't changed
         if (state.previousDoorState != data?.door?.status) {
-        	log.debug "door status has changed from '${state.previousDoorState}' to '${data?.door?.status}'. sending new event"
-        	result << createEvent(name: "door", value: data?.door?.status)
+            log.debug "door status has changed from '${state.previousDoorState}' to '${data?.door?.status}'. sending new event"
+            result << createEvent(name: "door", value: data?.door?.status)
             state.previousDoorState = data?.door?.status
         }
         result << createEvent(name: "temperature", value: data?.temperature, unit: "F")
@@ -179,32 +179,32 @@ def open() {
     def secret = sha256HashHex(pass)
     log.debug secret
     def hubaction = new physicalgraph.device.HubAction(
-    	method: "GET",
-    	path: "/door/open&${secret}",
-    	headers: [
-        	HOST: "${getHostAddress()}"
-    	]
-	)
+        method: "GET",
+        path: "/door/open&${secret}",
+        headers: [
+            HOST: "${getHostAddress()}"
+        ]
+    )
     return hubaction
 }
 
 def close() {
-	String pass = "${state.nonce}" + settings.password
+    String pass = "${state.nonce}" + settings.password
     def secret = sha256HashHex(pass)
     log.debug secret
     def hubaction = new physicalgraph.device.HubAction(
-    	method: "GET",
-    	path: "/door/close&${secret}",
-    	headers: [
-        	HOST: "${getHostAddress()}"
-    	]
-	)
+        method: "GET",
+        path: "/door/close&${secret}",
+        headers: [
+            HOST: "${getHostAddress()}"
+        ]
+    )
     return hubaction
 }
 
 def getCelsius(String fahrenheit) {
-	def celsius = ((Float.parseFloat(fahrenheit)-32) *  (5/9))
-	return celsius.trunc(1)
+    def celsius = ((Float.parseFloat(fahrenheit)-32) *  (5/9))
+    return celsius.trunc(1)
 }
 
 
